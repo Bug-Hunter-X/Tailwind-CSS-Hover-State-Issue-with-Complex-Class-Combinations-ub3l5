4e5a0af7d72804d80ea357a6e71e977e8bd98bf5
BugHunter-X <askzenai@gmail.com>
2025-01-23 18:36:05 +0100

    Add bug.js file

---
+++ bug.js
@@ -0,0 +1,7 @@
+```javascript
+// some javascript code that uses tailwindcss classes
+/* This is a comment that shows how to use tailwindcss classes */
+<div class="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded">
+  Hover over me!
+</div>
+```

7e7f39a1230c4bed2503532182188847d01edaf9
BugHunter-X <askzenai@gmail.com>
2025-01-23 18:36:06 +0100

    Add bugSolution.js file

---
+++ bugSolution.js
@@ -0,0 +1,7 @@
+```javascript
+// solution code that fixes the bug
+/*This is a comment that shows how to fix the bug*/
+<div class="bg-red-500 text-white font-bold py-2 px-4 rounded transition duration-300 ease-in-out hover:bg-red-700">
+  Hover over me!
+</div>
+```
